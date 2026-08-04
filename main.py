b6e05ae053cb17356643b7bb0fb483001a05ada6
qsc20001102 <qsc20001102@163.com>
2026-08-04 11:29:37 +0800

    33

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from .core.fun_basic import load_as_base64
 @register("astrbot_plugin_jx3", 
           "fxdyz", 
           "通过调用剑网三API接口获取游戏数据，处理发送。", 
-          "3.1",
+          "3.2.0",
           "https://github.com/qsc20001102/astrbot_plugin_jx3"
 )
 class Jx3ApiPlugin(Star):
